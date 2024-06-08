63aa46c799ed0577d52539955df85460007a147b
Huy0110 <duchuy01102001@gmail.com0>
2024-06-08 16:45:45 +0700

    Add list farm

---
+++ src/Pages/Farm/Farm.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../../Components/Navbar/Navbar";
+import Footer from "../../Components/Footer/Footer";
+import ListFarm from "../../Components/Farm/ListFarm";
+const Farm = () => {
+  return (
+    <div>
+      <Navbar />
+      <ListFarm />
+      <Footer/>
+    </div>
+  );
+};
+
+export default Farm;
