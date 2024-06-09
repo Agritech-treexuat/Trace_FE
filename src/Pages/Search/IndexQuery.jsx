fe9af91fa537b39134a179bbb797a48aec26cc0e
Huy0110 <duchuy01102001@gmail.com0>
2024-06-09 18:37:11 +0700

    Add query project index

---
+++ src/Pages/Search/IndexQuery.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../../Components/Navbar/Navbar'
+import SearchId from '../../Components/Traceanility/SearchId'
+import Footer from '../../Components/Footer/Footer'
+
+const IndexQuery = () => {
+  return (
+    <>
+        <Navbar />
+        <SearchId />
+        <Footer />
+    </>
+  )
+}
+
+export default IndexQuery
